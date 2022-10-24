97a4e9245ef9735b552c5d2c009463c69fa7a873
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-10-24 20:08:17 +0200

    Estructura Firebase

--- app/src/main/java/com/example/zenhabit/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/zenhabit/Activities/LoginActivity.kt
@@ -3,14 +3,28 @@ package com.example.zenhabit.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.zenhabit.R
+import com.example.zenhabit.databinding.ActivityLoginBinding
+import com.example.zenhabit.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var bin: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        bin = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(bin.root)
+
+        auth = Firebase.auth
+
+        bin.btnEne.setOnClickListener {
+            IdentificarUsuari(
+                bin.inputEmail.text.toString(),
+                bin.inputPsw.text.toString())
+        }
     }
 }
--- app/src/main/java/com/example/zenhabit/Activities/RegisterActivity.kt
+++ app/src/main/java/com/example/zenhabit/Activities/RegisterActivity.kt
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.zenhabit.R
+import com.example.zenhabit.databinding.ActivityLoginBinding
+import com.example.zenhabit.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var bin: ActivityRegisterBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        bin = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(bin.root)
+
+        auth = Firebase.auth
 
         val buttonRegister: Button = findViewById(R.id.btn_register)
         buttonRegister.setOnClickListener {
@@ -25,6 +34,14 @@ class RegisterActivity : AppCompatActivity() {
         buttonCancel.setOnClickListener {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        buttonRegister.setOnClickListener {
+            crearUsuari(
+                bin.inputCreateEmail.text.toString(),
+                bin.inputCreatePsw.text.toString(),
+                bin.inputCreateUserName.text.toString()
+            )
         }
 
 
