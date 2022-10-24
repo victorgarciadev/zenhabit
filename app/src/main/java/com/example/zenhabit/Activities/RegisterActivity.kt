94c0cf55cc0865a9dab46530deff88b8f64ac773
Izan Jimenez <jimenez.moreno.izan@alumnat.copernic.cat>
2022-10-24 19:36:28 +0200

    24/10/2022 -> Binding RegisterActivity.kt

--- app/src/main/java/com/example/zenhabit/Activities/RegisterActivity.kt
+++ app/src/main/java/com/example/zenhabit/Activities/RegisterActivity.kt
@@ -3,10 +3,18 @@ package com.example.zenhabit.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.zenhabit.R
+import com.example.zenhabit.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_register)
     }
+
+
 }
