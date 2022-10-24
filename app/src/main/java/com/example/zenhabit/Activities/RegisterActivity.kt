1790b18e03403c423d973290bbe5eab27edba438
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-10-24 19:37:19 +0200

    Intents

--- app/src/main/java/com/example/zenhabit/Activities/RegisterActivity.kt
+++ app/src/main/java/com/example/zenhabit/Activities/RegisterActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.zenhabit.R
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -18,10 +22,12 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         val buttonCancel: Button = findViewById(R.id.btn_cancel)
-        buttonRegister.setOnClickListener {
+        buttonCancel.setOnClickListener {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
         }
+
+
 
     }
 }
