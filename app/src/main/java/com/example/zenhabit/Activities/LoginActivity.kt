0f47cb6869fa8e7f2b7fa1c03a5119598cac7d7a
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-10-24 20:55:23 +0200

    Creació usuaris

--- app/src/main/java/com/example/zenhabit/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/zenhabit/Activities/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.example.zenhabit.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.zenhabit.R
 import com.example.zenhabit.databinding.ActivityLoginBinding
 import com.example.zenhabit.databinding.ActivityRegisterBinding
