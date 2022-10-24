06617a2a82dee7e833a388b0eb652b04e15df5e5
Txell Llanas <llanas.girones.meritxell@alumnat.copernic.cat>
2022-10-24 20:27:13 +0200

    20/10/2022 -> Crear SPINNER pantalles 'Crear/Editar' TASCA, HÀBIT + INTENT Login

--- app/src/main/java/com/example/zenhabit/Activities/SplashScreen.kt
+++ app/src/main/java/com/example/zenhabit/Activities/SplashScreen.kt
@@ -1,12 +1,24 @@
 package com.example.zenhabit.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.ActionBar
+import com.example.zenhabit.MainActivity
 import com.example.zenhabit.R
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.hide()
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this@SplashScreen, LoginActivity::class.java))
+            finish()
+        }, 4000)
     }
 }
