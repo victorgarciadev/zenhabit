3fb906392fce00fc729ac2181df593a4c2b6487d
Txell Llanas <llanas.girones.meritxell@alumnat.copernic.cat>
2022-10-25 21:32:25 +0200

    Update changed files

--- app/src/main/java/com/example/zenhabit/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/zenhabit/Activities/LoginActivity.kt
@@ -1,16 +1,52 @@
 package com.example.zenhabit.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.zenhabit.R
+import com.example.zenhabit.MainActivity
+import com.example.zenhabit.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
+    // Declarar variable pel View Binding
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        // Inicialitzar View Binding (Activity)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Intent cap a la pantalla principal 'Home' si l'usuari no està loguejat
+        binding.btnEnter.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.textViewRegisterLink.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    // Comprovar si usuari ja està loguejat
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            binding.btnEnter.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
 }
