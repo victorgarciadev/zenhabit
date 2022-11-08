1f76863820cb1687deb19f4e179c0c390821c8cd
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-11-08 21:37:25 +0100

    Funcionalitats tasca 7

---
+++ app/src/main/java/com/example/zenhabit/Fragments/HomeFragment.kt
@@ -0,0 +1,41 @@
+package com.example.zenhabit.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.zenhabit.databinding.ActivityMainBinding
+import com.example.zenhabit.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [home.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class home : Fragment() {
+
+    private var bin: FragmentHomeBinding? = null
+
+    private val binding get() = bin!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        bin = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
