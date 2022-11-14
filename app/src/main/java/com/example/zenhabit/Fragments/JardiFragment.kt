750f47ac134930d35b05f69f9407f139e8143b35
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-11-14 20:57:06 +0100

    nav home

--- app/src/main/java/com/example/zenhabit/Fragments/JardiFragment.kt
+++ app/src/main/java/com/example/zenhabit/Fragments/JardiFragment.kt
@@ -6,28 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.zenhabit.R
+import com.example.zenhabit.databinding.FragmentJardiBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [JardiFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class JardiFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var bin: FragmentJardiBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        bin = FragmentJardiBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
@@ -36,25 +28,5 @@ class JardiFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_jardi, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JardiFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            JardiFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
