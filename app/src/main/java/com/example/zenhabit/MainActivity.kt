c2d545ac6df009e0481e19b7e0edcca0f24a20d6
Victor Garcia <garcia.pulpon.victor@alumnat.copernic.cat>
2022-11-15 21:28:09 +0100

    15/11/2022 -> Bottom Menu implementado y funcional, Action Bar (ajustes) a medias

--- app/src/main/java/com/example/zenhabit/MainActivity.kt
+++ app/src/main/java/com/example/zenhabit/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.example.zenhabit
 
-import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.util.Log
-import android.widget.Button
 import com.example.zenhabit.databinding.ActivityMainBinding
 import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.Fragment
@@ -17,11 +10,13 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.zenhabit.Fragments.JardiFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
     private lateinit var bin: ActivityMainBinding
+    lateinit var bottomNavigation : BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +32,17 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController)
 
         val actionBar: ActionBar? = supportActionBar
+        //actionBar?.setCustomView(R.menu.menu_action_bar)
         actionBar?.hide()
+
+        bottomNavigation = bin.bottomNavigationView as BottomNavigationView
+        bottomNavigation.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.home -> navController.navigate(R.id.homeActivity)
+                R.id.settings -> navController.navigate(R.id.settingsFragment)
+            }
+            true
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
