c109f90440f4806488fe378ba28a1dcdf6d08036
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-11-24 17:20:13 +0100

    canvi contrasenya

--- app/src/main/java/com/example/zenhabit/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/zenhabit/Fragments/SettingsFragment.kt
@@ -2,12 +2,18 @@ package com.example.zenhabit.Fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.example.zenhabit.Activities.LoginActivity
 import com.example.zenhabit.MainActivity
+import com.example.zenhabit.R
 import com.example.zenhabit.databinding.FragmentSettingsBinding
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
@@ -31,27 +37,54 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val user = FirebaseAuth.getInstance().currentUser
         // Inflate the layout for this fragment
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         (activity as AppCompatActivity?)!!.supportActionBar?.setTitle("Configuració")
-        binding.btnCancel.setOnClickListener {
-            val intent = Intent(context, MainActivity::class.java)
-            startActivity(intent)
-        }
         binding.btnSave.setOnClickListener {
-            val newUsername = binding.inputChangeUserName.text
-            if (!newUsername.isEmpty()) {
+            Log.d("0","entra button")
+            val actualUser = FirebaseAuth.getInstance().currentUser
+            val email = actualUser!!.email
+            val actualPsw = binding.inputActualPsw.text
+            if (!actualPsw.isEmpty()) {
+                val credential = EmailAuthProvider.getCredential(email!!, actualPsw.toString())
+                Log.d("1","Entra if")
+                //actualUser.reauthenticate(credential)
+                actualUser.reauthenticate(credential).addOnCompleteListener {
+                    actualUser.updatePassword(binding.inputChangePsw.text.toString()).addOnCompleteListener { task ->
+                        Log.d("2","Entra update")
+                        if (task.isSuccessful) {
+                            Toast(activity).showCustomToast("Contrasenya canviada correctament.")
+                        } else {
+                            Toast(activity).showCustomToast("Error al canviar contrasenya.")
+                        }
+                    }
+                }
 
             }
-            val intent = Intent(context, MainActivity::class.java)
-            startActivity(intent)
         }
 
         activity?.actionBar?.show()
 
         return binding.root
 
+    }
+
+    private fun Toast.showCustomToast(message: String)
+    {
+        val layout = requireActivity().layoutInflater.inflate (
+            R.layout.toast_layout,
+            requireActivity().findViewById(R.id.toast_container)
+        )
+
+        val textView = layout.findViewById<TextView>(R.id.toast_text)
+        textView.text = message
+
+        this.apply {
+            setGravity(Gravity.CENTER, 0, 700)
+            duration = Toast.LENGTH_LONG
+            view = layout
+            show()
+        }
     }
 
 }
