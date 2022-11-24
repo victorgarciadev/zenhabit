26d502f56eff68a21d0ed49843fb447cdd2df96b
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-11-24 21:38:32 +0100

    insercio a base de dates

---
+++ app/src/main/java/com/example/zenhabit/models/Planta.kt
@@ -0,0 +1,8 @@
+package com.example.zenhabit.models
+
+class Planta(comAconseguir: String, descripcio: String, icona: String, tipus: String) {
+    val comAconseguir = comAconseguir
+    val descripcio = descripcio
+    val icona = icona
+    val tipus = tipus
+}
---
+++ app/src/main/java/com/example/zenhabit/models/PlantaUsuari.kt
@@ -0,0 +1,9 @@
+package com.example.zenhabit.models
+
+import com.google.type.Date
+
+class PlantaUsuari(planta: Planta, quantitat: Int, data: Date) {
+    val planta = planta
+    val quantitat = quantitat
+    val data = data
+}
