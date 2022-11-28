f4011eaa83381762130fd8cd491062347c7c8fb5
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-11-28 18:12:16 +0100

    parche temporal

--- app/src/main/java/com/example/zenhabit/models/Tasca.kt
+++ app/src/main/java/com/example/zenhabit/models/Tasca.kt
@@ -2,11 +2,12 @@ package com.example.zenhabit.models
 
 import com.google.type.Date
 
-class Tasca(name: String, time: String, descripcio: String?, categoria: CategoriaTipusHabit, complert: Boolean, dataAconseguit: Date?) {
+class Tasca(name: String, time: String, tipus: String /*descripcio: String?, categoria: CategoriaTipusHabit?, complert: Boolean?, dataAconseguit: Date?*/) {
     val nom = name
     val hora = time
-    val descripcio = descripcio
-    val categoria = categoria
-    val complert = complert
-    val dataAconseguit = dataAconseguit
+    val tipus = tipus
+//    val descripcio = descripcio
+//    val categoria = categoria
+//    val complert = complert
+//    val dataAconseguit = dataAconseguit
 }
