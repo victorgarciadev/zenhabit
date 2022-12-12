4352f8dff75e9df333c87e6ba35f6426ac28d519
Pablo Morante <morante.garcia.pablo@alumnat.copernic.cat>
2022-12-12 18:31:03 +0100

    implementacio canvi de nom d'usuari settings / millora no internet activity / creacio habit amb nom

---
+++ app/src/main/java/com/example/zenhabit/models/Objectiu.kt
@@ -0,0 +1,15 @@
+package com.example.zenhabit.models
+
+import java.util.Date
+
+class Objectiu(nom: String, descripcio: String, categoria: String, dataLimit: Date, repetible: List<Dies>,horari:String, complert: Boolean, ultimaDataFet: Date, tipus: Boolean) {
+    val nom = nom
+    val descripcio = descripcio
+    val categoria = categoria
+    val dataLimit = dataLimit
+    val repetible = repetible
+    val horari = horari
+    val complert = complert
+    val ultimaDataFet = ultimaDataFet
+    val tipus = tipus
+}
