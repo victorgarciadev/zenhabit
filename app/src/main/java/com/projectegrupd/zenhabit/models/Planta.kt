3685da582b4a3fd2890352c354016ba05a94b327
Victor Garcia <garcia.pulpon.victor@alumnat.copernic.cat>
2023-01-10 15:52:30 +0100

    10/01/2023 -> preparación para Play Store

---
+++ app/src/main/java/com/projectegrupd/zenhabit/models/Planta.kt
@@ -0,0 +1,10 @@
+package com.projectegrupd.zenhabit.models
+
+/**
+ * @author Pablo Morante
+ */
+class Planta(nom: String, nombre: String, name: String) {
+    val nom = nom
+    val nombre = nombre
+    val name = name
+}
